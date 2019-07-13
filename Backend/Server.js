a352d621512d98ed8257b7de8d7fcab61790aa7f
Gregory Rutherford <gregorford@gmail.com>
2019-07-13 10:06:24 -0500

    added server process

--- Backend/Server.js
+++ Backend/Server.js
@@ -17,6 +17,10 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
 
 
